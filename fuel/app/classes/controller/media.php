396d9779bbdd6c342cf432adc908a915b29fea8b
erwinp <erwin.paglinawan@gmail.com>
2014-05-29 21:48:25 +0800

    Added media

---
+++ fuel/app/classes/controller/media.php
@@ -0,0 +1,21 @@
+<?php
+
+class Controller_Media extends Controller_Rest
+{
+    protected $format = "json";
+
+    public function action_index() {
+        $mediaId = Input::get("media_id");
+
+        $fullUrl = Config::get('drupal_views');
+        $fullUrl .= "/media?media_id=$mediaId";
+
+	$curl = Request::forge($fullUrl, "curl");
+        $curl->execute();
+        $data = $curl->response();
+
+        $this->response($data);
+    }
+}
+
+?>
---
+++ fuel/app/logs/2014/05/29.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2014-05-29 21:47:45 --> Notice - Undefined variable: media_id in /Users/erwinp/Sites/amt-api/fuel/app/classes/controller/media.php on line 11

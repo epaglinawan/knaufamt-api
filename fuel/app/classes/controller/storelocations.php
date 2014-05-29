f446b4a4abe6f10d0909a0f3b456c7bacf4565b4
erwinp <erwin.paglinawan@gmail.com>
2014-05-29 21:25:59 +0800

    Added store locations

---
+++ fuel/app/classes/controller/storelocations.php
@@ -0,0 +1,21 @@
+<?php
+
+class Controller_Storelocations extends Controller_Rest
+{
+    protected $format = "json";
+
+    public function action_index() {
+        $timestamp = Input::get("timestamp");
+
+        $fullUrl = Config::get('drupal_views');
+        $fullUrl .= "/store_locations?timestamp=$timestamp";
+
+	$curl = Request::forge($fullUrl, "curl");
+        $curl->execute();
+        $data = $curl->response();
+
+        $this->response($data);
+    }
+}
+
+?>
